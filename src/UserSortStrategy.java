2cc678f7424afb26e99459f6b90ae305db434df2
NostraDamskii <kekorov@gmail.com>
2026-07-27 20:18:02 +0300

    добавила сортер и стратегию

---
+++ src/UserSortStrategy.java
@@ -0,0 +1,45 @@
+package src;
+
+import java.util.Comparator;
+
+public enum UserSortStrategy implements Comparator<User> {
+    BY_NAME_ASC {
+        @Override
+        public int compare(User u1, User u2) {
+            return u1.getName().compareToIgnoreCase(u2.getName());
+        }
+    },
+    BY_NAME_DESC {
+        @Override
+        public int compare(User u1, User u2) {
+            return u2.getName().compareToIgnoreCase(u1.getName());
+        }
+    },
+
+    BY_EMAIL_ASC {
+        @Override
+        public int compare(User u1, User u2) {
+            return u1.getEmail().compareToIgnoreCase(u2.getEmail());
+        }
+    },
+    BY_EMAIL_DESC {
+        @Override
+        public int compare(User u1, User u2) {
+            return u2.getEmail().compareToIgnoreCase(u1.getEmail());
+        }
+    },
+
+    BY_PASSWORD_ASC {
+        @Override
+        public int compare(User u1, User u2) {
+            return Integer.compare(u1.getPassword(), u2.getPassword());
+        }
+    },
+    BY_PASSWORD_DESC {
+        @Override
+        public int compare(User u1, User u2) {
+            return Integer.compare(u2.getPassword(), u1.getPassword());
+        }
+    },
+
+}
---
+++ src/UserSorter.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserSorter {
+    public List<User> sort(List<User> users, UserSortStrategy strategy) {
+        if (users == null || users.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return users.stream()
+                .sorted(strategy)
+                .collect(Collectors.toList());
+    }
+}
