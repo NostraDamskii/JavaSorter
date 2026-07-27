94871c50e85384af570fd0c537967f1f3b4e0fcc
Evgeniy <evstigneev_2024@mail.ru>
2026-07-27 11:02:18 +0700

    Доп 1, Доп 2.

--- src/RandomGen.java
+++ src/RandomGen.java
@@ -16,7 +16,7 @@ public class RandomGen
         String name = NAMES[random.nextInt(NAMES.length)];
         String email = EMAILS[random.nextInt(EMAILS.length)];
         // Случайная генерация пароля с помощью метода встроенного класса java Random .nextInt <метод класса random который возвращает случайное значение в пределах указанного диапазона
-        int password = random.nextInt(100000, 999999);;
+        int password = random.nextInt(100000, 999999);
         // Возвращаем пользователя со случайными данными
         return new User.Builder().name(name).email(email).password(password).build();
     }
