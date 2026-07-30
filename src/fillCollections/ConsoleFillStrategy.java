e5edca2a4e7d33222abea3b2c46f5fea91f9a548
s.shagavsky <sergeyfox200214@gmail.com>
2026-07-31 00:20:05 +0300

    Исправил валидацию при ручном вводе

--- src/fillCollections/ConsoleFillStrategy.java
+++ src/fillCollections/ConsoleFillStrategy.java
@@ -3,7 +3,6 @@ package src.fillCollections;
 import src.CustomLinkedList;
 import src.user.User;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleFillStrategy implements UserFillStrategy {
@@ -18,12 +17,15 @@ public class ConsoleFillStrategy implements UserFillStrategy {
   @Override
   public void fill(CustomLinkedList<User> users) {
     System.out.println("Заполнение вручную (" + count + " пользователей):");
+
     for (int i = 0; i < count; i++) {
+
       System.out.println("\nПользователь #" + (i + 1));
+
+      // ===== Ввод имени =====
       String name;
 
       while (true) {
-
         System.out.print("Введите имя: ");
         name = scanner.nextLine();
 
@@ -34,50 +36,52 @@ public class ConsoleFillStrategy implements UserFillStrategy {
         System.out.println("Ошибка! Имя не может быть пустым.");
       }
 
-      System.out.print("Введите пароль (число): ");
+      // ===== Ввод пароля =====
       int password;
 
       while (true) {
-
         System.out.print("Введите пароль (число): ");
 
         if (scanner.hasNextInt()) {
 
           password = scanner.nextInt();
-          scanner.nextLine();
+          scanner.nextLine(); // очищаем символ Enter
 
           if (password > 0) {
             break;
           }
 
-          System.out.println("Пароль должен быть больше 0.");
+          System.out.println("Ошибка! Пароль должен быть больше 0.");
+
         } else {
 
-          System.out.println("Введите целое число!");
+          System.out.println("Ошибка! Введите целое число.");
           scanner.nextLine();
         }
       }
-      scanner.nextLine();
 
-      System.out.print("Введите e-mail: ");
+      // ===== Ввод e-mail =====
       String mail;
 
       while (true) {
-
         System.out.print("Введите e-mail: ");
         mail = scanner.nextLine();
 
         if (!mail.isBlank()
-            && mail.contains("@")
-            && mail.contains(".")) {
-
+                && mail.contains("@")
+                && mail.contains(".")) {
           break;
         }
 
-        System.out.println("Некорректный e-mail.");
+        System.out.println("Ошибка! Некорректный e-mail.");
       }
 
-      User user = new User.Builder().name(name).password(password).email(mail).build();
+      User user = new User.Builder()
+              .name(name)
+              .password(password)
+              .email(mail)
+              .build();
+
       users.add(user);
     }
   }
